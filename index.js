2640596ec193731fe2cbe852bcfdbc1bad77e889
Ali <iamaliahad@outlook.com>
2022-08-10 14:03:19 +0800

    comment modified

--- index.js
+++ index.js
@@ -6,13 +6,18 @@ import userRoutes from './routes/users.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+// middleware
 app.use(express.json());
 
+//routes
 app.use('/api/users', userRoutes)
 
+
+// connect to the database
 const connectDB = async()=>{
     try{
-        mongoose.connect(process.env.DB_CONNECTION);
+        mongoose.connect(process.env.DB_CONNECTION); 
         console.log('MongoDB Connected...');
     } catch(err){
         console.log(err.message);
